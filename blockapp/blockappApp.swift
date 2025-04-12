5816e1eb2ef1440e9766b4ffda8e73dee4a82c19
Jan Osuský <codes.osusky@outlook.com>
2025-04-12 15:58:42 +0200

    Initial Commit

---
+++ blockapp/blockappApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockappApp.swift
+//  blockapp
+//
+//  Created by Jan Osuský on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct blockappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
